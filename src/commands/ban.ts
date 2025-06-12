709a4d41a5acc64f090e09578ea2e4616b59c57c
Joshua <joshua.ghattas@gmail.com>
2025-06-12 06:10:48 -0500

    Fixed ban and unban commands

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -1,7 +1,7 @@
 import { Command } from "../types";
 import config from "../config.json";
 import { getMemoryFilePath, retrieveJSONData, setJSONData } from "../lib/base-memory-functions";
-import path from "path"
+import path from "path";
 
 const command: Command = {
   name: "ban",
@@ -16,33 +16,38 @@ const command: Command = {
   ],
   execute: async (interaction) => {
     const userToBan = interaction.options.getUser("user");
+
     try {
-      if (config.admins.includes(interaction.user.id)) {
-        if (config.admins.includes(userToBan.id)) {
-          return await interaction.reply(
-            "You cannot ban another admin. Please choose a different user."
-          );
-        } else {
-          await interaction.reply(`Banning <@${userToBan.id}>...`);
-          let userData = await retrieveJSONData(path.join(getMemoryFilePath(), "users", userToBan.id), "userData.json")
-          if (userData.banned) {
-            return await interaction.reply("User is already banned")
-          } else {
-            userData.banned = true
-            setJSONData(path.join(getMemoryFilePath(), "users", userToBan.id), "userData.json", userData)
-            return await interaction.reply("Succesfully banned user")
-          }
-        }
-      } else {
+      if (!config.admins.includes(interaction.user.id)) {
         return await interaction.reply(
           "You do not have permission to use this command. Only T3D admins can ban users."
         );
       }
+
+      if (config.admins.includes(userToBan.id)) {
+        return await interaction.reply(
+          "You cannot ban another admin. Please choose a different user."
+        );
+      }
+
+      const userPath = path.join(getMemoryFilePath(), "users", userToBan.id);
+      let userData = await retrieveJSONData(userPath, "userData.json");
+
+      if (userData?.banned) {
+        return await interaction.reply("User is already banned.");
+      }
+
+      userData.banned = true;
+      await setJSONData(userPath, "userData.json", userData);
+
+      return await interaction.reply(`Successfully banned <@${userToBan.id}>.`);
     } catch (error) {
       console.error("Error executing ban command:", error);
-      return await interaction.reply(
-        "An error occurred while trying to execute the ban command."
-      );
+      if (!interaction.replied) {
+        return await interaction.reply("An error occurred while executing the ban command.");
+      } else {
+        return await interaction.followUp("An error occurred while executing the ban command.");
+      }
     }
   },
 };
--- src/commands/unban.ts
+++ src/commands/unban.ts
@@ -20,40 +20,50 @@ const command: Command = {
   ],
   execute: async (interaction) => {
     const userToBan = interaction.options.getUser("user");
+
     try {
-      if (config.admins.includes(interaction.user.id)) {
-        if (config.admins.includes(userToBan.id)) {
-          return await interaction.reply(
-            "Admins cannot have been banned in the first place."
-          );
-        } else {
-          await interaction.reply(`Unbanning <@${userToBan.id}>...`);
-          let userData = await retrieveJSONData(
-            path.join(getMemoryFilePath(), "users", userToBan.id),
-            "userData.json"
-          );
-          if (!userData.banned) {
-            return await interaction.reply("User is already unbanned");
-          } else {
-            userData.banned = false;
-            setJSONData(
-              path.join(getMemoryFilePath(), "users", userToBan.id),
-              "userData.json",
-              userData
-            );
-            return await interaction.reply("Succesfully unbanned user");
-          }
-        }
-      } else {
+      if (!config.admins.includes(interaction.user.id)) {
         return await interaction.reply(
           "You do not have permission to use this command. Only T3D admins can unban users."
         );
       }
-    } catch (error) {
-      console.error("Error executing ban command:", error);
-      return await interaction.reply(
-        "An error occurred while trying to execute the unban command."
+
+      if (config.admins.includes(userToBan.id)) {
+        return await interaction.reply(
+          "Admins cannot have been banned in the first place."
+        );
+      }
+
+      const userData = await retrieveJSONData(
+        path.join(getMemoryFilePath(), "users", userToBan.id),
+        "userData.json"
       );
+
+      if (!userData?.banned) {
+        return await interaction.reply("User is already unbanned.");
+      }
+
+      userData.banned = false;
+      await setJSONData(
+        path.join(getMemoryFilePath(), "users", userToBan.id),
+        "userData.json",
+        userData
+      );
+
+      return await interaction.reply(
+        `Successfully unbanned <@${userToBan.id}>.`
+      );
+    } catch (error) {
+      console.error("Error executing unban command:", error);
+      if (!interaction.replied) {
+        return await interaction.reply(
+          "An error occurred while trying to execute the unban command."
+        );
+      } else {
+        return await interaction.followUp(
+          "An error occurred while trying to execute the unban command."
+        );
+      }
     }
   },
 };
